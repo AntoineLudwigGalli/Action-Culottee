21d58facd11c2d224b5519a9d6070ac1669ac9c8
Antoine <antoine@gmail.com>
2022-05-19 00:03:25 +0200

    ajout page agenda, sa table et son form admin sans verif ni front

---
+++ src/Entity/FutureEvents.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FutureEventsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FutureEventsRepository::class)]
+class FutureEvents
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'datetime')]
+    private $eventDate;
+
+    #[ORM\Column(type: 'string', length: 300)]
+    private $eventDescription;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEventDate(): ?\DateTimeInterface
+    {
+        return $this->eventDate;
+    }
+
+    public function setEventDate(\DateTimeInterface $eventDate): self
+    {
+        $this->eventDate = $eventDate;
+
+        return $this;
+    }
+
+    public function getEventDescription(): ?string
+    {
+        return $this->eventDescription;
+    }
+
+    public function setEventDescription(string $eventDescription): self
+    {
+        $this->eventDescription = $eventDescription;
+
+        return $this;
+    }
+}
